f222308e70e5fb53e21404521942c4108b20e68f
Noegeo <noegeo01@gmail.com>
2022-02-23 21:39:27 +0100

    r

--- dwc/UT09/aleatorio.js
+++ dwc/UT09/aleatorio.js
@@ -1,14 +1,23 @@
 function entre(x,y){
-    return Math.round(Math.random()*(y-x+1)+x)
+    return Math.round(Math.random()*(y-x-1)+x)
 }
-function parEntre(x,y){
+ function parEntre(x,y){
     let par=1;
     while(par%2!==0){
         par=entre(x,y)
     }
     return par;
 }
-function imparEntre(x,y){
-    
-    return par(x,y)+1;
+ function imparEntre(x,y){
+    let impar=2;
+    while(par%2==0){
+        par=entre(x,y)
+    }
+    return impar;
+}
+const cero =0;
+export {
+    parEntre,
+    imparEntre,
+    cero
 }
